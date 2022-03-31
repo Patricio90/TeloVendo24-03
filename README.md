1c1a6182ed1fcf23e3df1cb381ef11150db94fc5
Patricio90 <90427375+Patricio90@users.noreply.github.com>
2022-03-31 01:54:23 -0300

    Update Repository

---
+++ README.md
@@ -0,0 +1,1 @@
+# TeloVendo24-03
---
+++ js/app.js
@@ -0,0 +1,29 @@
+//función que muestra fecha en footer de páginas.
+let r = new Date();
+document.getElementById("Fecha").innerHTML=r;
+
+//Función que valida datos en formulario
+
+function validarFormulario(){
+console.log("Completar datos");
+    let lfname = document.getElementById("Nombre").value;
+    let lemail = document.getElementById("Correo").value;
+    let lmessage = document.getElementById("Mensaje").value;
+    console.log("Completar datos");
+    console.log("Nombre" + lfname);
+    console.log("Correo" + lemail);
+    console.log("Mensaje" + lmessage);
+
+//Validación de datos
+
+if ((lfname.length == 0) || (lemail.length == 0) || (lmessage.length == 0))
+{
+    alert('Favor completar los campos faltantes');
+}
+else
+{
+    alert('Hemos recibido su mensaje, nuestro equipo se pondrá en contacto con usted');
+}
+};
+
+
